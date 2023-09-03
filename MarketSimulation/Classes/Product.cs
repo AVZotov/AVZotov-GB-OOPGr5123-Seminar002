4fe3e61c49c8e2ade3559580fb95718ed9cc2103
Alexey Zotov <alexey.zotov@ymail.com>
2023-09-03 23:39:59 +0300

    Product class added

--- MarketSimulation/Classes/Product.cs
+++ MarketSimulation/Classes/Product.cs
@@ -2,5 +2,38 @@
 {
     internal class Product
     {
+        private readonly Guid _id;
+
+        public Product()
+        {
+            _id = Guid.NewGuid();
+        }
+
+        public Guid GetId()
+        {
+            return _id;
+        }
+
+        public override bool Equals(object obj)
+        {
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Product other = (Product)obj;
+            return this._id.Equals(other.GetId());
+        }
+
+        public override int GetHashCode()
+        {
+            return _id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Product: {_id.ToString()[..8]}";
+        }
     }
 }
